355b9e3e94549cdfd73472287f807fb1c48c4730
hideaki <hs.wgp.46@gmail.com>
2022-10-15 18:16:45 +0900

    Angularフロントエンド側作成

---
+++ src/app/products.ts
@@ -0,0 +1,51 @@
+export const products = [
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone XL',
+    price: 799,
+    description:'A large phone with one of the best screens',
+    heding1: 'Heading 1',
+    heding2: 'Heading 2',
+    heding3: 'Heading 3',
+    hedingtext1: '1:Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+    hedingtext2: '2:Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+    hedingtext3: '3:Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.'
+
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone Mini',
+    price: 699,
+    description:'A great phone with one of the best cameras',
+    heding1: 'Heading 1',
+    heding2: 'Heading 2',
+    heding3: 'Heading 3',
+    hedingtext1: '1:Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+    hedingtext2: '2:Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+    hedingtext3: '3:Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.'
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone Standard',
+    price: 299,
+    description:'',
+    heding1: 'Heading 1',
+    heding2: 'Heading 2',
+    heding3: 'Heading 3',
+    hedingtext1: '1:Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+    hedingtext2: '2:Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+    hedingtext3: '3:Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.'
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone Special',
+    price: 999,
+    description:'A great phone with one of the best cameras',
+    heding1: 'Heading 1',
+    heding2: 'Heading 2',
+    heding3: 'Heading 3',
+    hedingtext1: '1:Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+    hedingtext2: '2:Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+    hedingtext3: '3:Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.'
+  },
+]
