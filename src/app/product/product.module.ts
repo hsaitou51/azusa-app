0adbc38012b9c4379281f5777f598731355b1de0
hideaki <hs.wgp.46@gmail.com>
2022-11-02 23:32:46 +0900

    Heroku用セットアップ

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
 				{ path: '', component: ProductListingsComponent },
 				{ path: ':productId', component: ProductDetailComponent }
 			 ] 
-    },
+    }
     ];
 
 @NgModule({
